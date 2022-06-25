5480d3208693b0011a6396b0eff0077777d98784
jakubglatki <jakub.glatki@gmail.com>
2022-06-26 00:59:08 +0100

    +added data files for all seasons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+import pandas as pd
 
 from AI.AIPrediction import AIPrediction
 from AI.DecisionTree import DecisionTree
@@ -9,32 +10,31 @@ from AI.TabNet import TabNet
 from AI.XGBoost import XGBoost
 
 if __name__ == '__main__':
-    # print(utilities.get_player_parameter(234, utilities.import_data('players_raw_17_18.csv'), 'now_cost'))
-    # players = basic_method_self_made.get_players_with_most_points()
-    # for index, player in players.iterrows():
-    #     print(utilities.get_player_parameter(player['id'], players, 'web_name'))
-    #basic_method_self_made.choose_team()
+    # players = pd.read_csv('data/players_raw_21_22.csv')
+    # players = players.add_suffix('_21/22')
+    # players.to_csv('data/players_21_22.csv')
+    # basic_method_self_made.choose_team()
     ai = AIPrediction()
     xgboost = XGBoost()
     predicted_points = xgboost.train_model()
     xgboost.choose_team_with_predicted_points(predicted_points)
 
-    tree = DecisionTree()
-    predicted_points = tree.train_model()
-    tree.choose_team_with_predicted_points(predicted_points)
-
-    randomForest = RandomForest()
-    predicted_points = randomForest.train_model()
-    randomForest.choose_team_with_predicted_points(predicted_points)
-
-    knn = KNN()
-    predicted_points = knn.train_model()
-    knn.choose_team_with_predicted_points(predicted_points)
-
-    linear_regression = LinearRegressionAI()
-    predicted_points = linear_regression.train_model()
-    linear_regression.choose_team_with_predicted_points(predicted_points)
-
-    tab_net = TabNet()
-    predicted_points = tab_net.train_model()
-    tab_net.choose_team_with_predicted_points(predicted_points)
+    # tree = DecisionTree()
+    # predicted_points = tree.train_model()
+    # tree.choose_team_with_predicted_points(predicted_points)
+    #
+    # randomForest = RandomForest()
+    # predicted_points = randomForest.train_model()
+    # randomForest.choose_team_with_predicted_points(predicted_points)
+    #
+    # knn = KNN()
+    # predicted_points = knn.train_model()
+    # knn.choose_team_with_predicted_points(predicted_points)
+    #
+    # linear_regression = LinearRegressionAI()
+    # predicted_points = linear_regression.train_model()
+    # linear_regression.choose_team_with_predicted_points(predicted_points)
+    #
+    # tab_net = TabNet()
+    # predicted_points = tab_net.train_model()
+    # tab_net.choose_team_with_predicted_points(predicted_points)
