debfe2c0d70c0e0c46797fdc3608bd6bec5bf216
Jakub Głatki <jakub.glatki@gmail.com>
2022-08-17 16:46:05 +0100

    #class for comparing models

--- compare_classes.py
+++ compare_classes.py
@@ -35,7 +35,7 @@ def check_all_algorithms(season):
 
 def compare_models():
     seasons = ['17_18', '18_19', '19_20', '20_21']
-    gameweeks= [1,9,18,27,36]
+    gameweeks= [9,18,27,36]
     models=['linear_regression','knn','decision_tree','random_forest','XGBoost','TabNet']
     columns_names=[]
     first_gw=1
@@ -45,11 +45,14 @@ def compare_models():
                 columns_names.append(model+'_'+season+'_gw'+str(gameweek))
 
     df = pd.DataFrame(columns=columns_names, index=["RMSE", "Number of points"])
-    for season in seasons:
-        for gameweek in gameweeks:
-            xgboost = XGBoost(season)
-            predicted_points = xgboost.train_model()
-            points = xgboost.choose_team_with_predicted_points(predicted_points)
-            rmse = xgboost.calculate_RMSE(xgboost.model, False)
-            d=3
+    # for season in seasons:
+    #     first_gw=2
+    #     for gameweek in gameweeks:
+    #         xgboost = XGBoost(season)
+    #         predicted_points = xgboost.train_model()
+    #         points = xgboost.choose_team_with_predicted_points(predicted_points)
+    #         rmse = xgboost.calculate_RMSE(xgboost.model, False)
+    #         Gameweeks_data_manipulation.add_gameweeks_data(first_gw, gameweek, season)
+    #         first_gw=gameweek
+    #         d=3
     xd=4
