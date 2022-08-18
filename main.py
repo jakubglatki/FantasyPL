60aac0d3bdf420c9a33aa31077bf874b7d3a26c2
Jakub Głatki <jakub.glatki@gmail.com>
2022-08-18 15:40:57 +0100

    text

--- main.py
+++ main.py
@@ -91,10 +91,19 @@ if __name__ == '__main__':
     predicted_points = tab_net.train_model()
     tab_net.choose_team_with_predicted_points(predicted_points)
 
+String
 text = "As visible in the Figure 5.3, models are varying stongly with their performence, in both metrics- RMSE and the number of points. There are some situations with the RMSE being low, "
 + "but the total number of points being relatively low as well. In most cases it is caused by correctly predicting most of the players, who did not get a lot of points, but failing at "
 + "predicting the positive outliers, the players with most points. Since the decision support system is supposed to help with choosing the best performing players,"
 + ", and not the average ones, the total number of points is a parameter which is more important in the decision making process, about choosing the best model."
 + "Another important metric in choosing the best model is the time when the model was making good predictions. The predictions made in the last two seasons,"
-+ "when the models had more data to learn from are more important than the predictions from the beginning, with limited informations. After taking all that "
-+ "into consideration, the MODEL was chosen for the detailed case study, showned in the next section."
++ "when the models had more data to learn from are more important than the predictions from the beginning, with limited informations. "
++ "Is is expected, that the machine learning models would perform better with having more data, but as showed in this example it is not true for all of them,"
++ "especially the linear regression. Beside expected better performence with each season it would also be expected, that the algorithms will perform better during"
++ "the course of the season, having more game-weeks to learn from. Some of the algorithms, like linear regression or KNN are performing visibly worse during the course of the season,"
++ "being unable to correctly identify how the new data should affect the prediction. From the other checked models, the decision tree is consisntent in its results, but they are worse than"
++ "the best performing models. Random forest, after bad first season started to perform much better, but got worse results at the end of the course. In some places it also"
++ "got single, very bad results in the most point category, like with 1849 points after game-week 9 of the 19/20 season, or 1831 points after game-week 27 of the 20/21 season. "
++ "TabNet's behaviour is similar to the one of the random forest, getting bettere with time, having some problems during the last season, and having some bad outliers in performence,"
++ "like 1872 points after 36 game-weeks of the 18/19 season, or 1852 points after game-week 9 of the 19/20 season. After taking all that "
++ "into consideration, the XGBoost was chosen for the detailed case study, showned in the next section.";
